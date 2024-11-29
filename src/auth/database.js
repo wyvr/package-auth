000c1bae22693705c04099b43b535b126a519ab7
wyvr <info@wyvr.dev>
2024-11-29 22:50:42 +0100

    chore: update to $src syntax

--- src/auth/database.js
+++ src/auth/database.js
@@ -1,7 +1,7 @@
-import { createConnection } from '@src/database/database.js';
+import { createConnection } from '$src/database/database.js';
 import { Cwd } from 'wyvr/src/vars/cwd.js';
 import { FOLDER_STORAGE } from 'wyvr/src/constants/folder.js';
-import { FILE } from '@src/auth/constants.js';
+import { FILE } from '$src/auth/constants.js';
 
 let connection;
 const path = Cwd.get(FOLDER_STORAGE, FILE);
