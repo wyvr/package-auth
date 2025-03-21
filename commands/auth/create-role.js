1f3ec95467014a553874a94799067466a530e4ae
wyvr <info@wyvr.dev>
2025-03-21 23:11:01 +0100

    wip: begin creating roles

--- commands/auth/create-role.js
+++ commands/auth/create-role.js
@@ -1,21 +1,23 @@
+import { createRole } from '$src/auth/resource/role.js';
 import { execute_flag_prompts } from 'wyvr/commands.js';
 import { logger } from 'wyvr/universal.js';
 
 export const meta = {
     desc: 'Create new role',
     flags: [
-        { key: 'role', desc: 'role' },
-        { key: 'name', desc: 'Name' }
-    ],
+        { key: 'name', desc: 'Name' },
+        { key: 'role', desc: 'role' }
+    ]
 };
 
 export async function execute(context) {
-    const result = await execute_flag_prompts(context?.cli?.flags, [
-        { key: 'role', name: 'Role', type: 'input', required: true },
-        { key: 'name', name: 'Name', type: 'input', required: true }
-    ]);
+    const name = await execute_flag_prompts(context?.cli?.flags, [{ key: 'name', name: 'Name', type: 'input', required: true }]);
 
-    // TODO create role in db
+    let role = name.name.toLowerCase().replace(/[^a-zA-Z0-9]+/g, '_');
+    if (context?.cli?.flags?.role) {
+        role = context.cli.flags.role;
+    }
 
-    console.log('result', result)
+    createRole(name.name, role);
+    logger.success('Role created');
 }
--- src/auth/resource/role.js
+++ src/auth/resource/role.js
@@ -1,8 +1,9 @@
 import { prompt_option } from 'wyvr/commands.js';
 
-export function getRoles() {
-
-}
+export function getRoles() {}
 export function getRolesAsPrompt() {
-    return [prompt_option('admin', 'Admin'), prompt_option('user', 'User')]
+    return [prompt_option('admin', 'Admin'), prompt_option('user', 'User')];
+}
+export function createRole(name, role) {
+    console.log('createRole', name, role);
 }
